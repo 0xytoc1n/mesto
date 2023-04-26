a73d722e817b227359009dc214f8539106657e08
Oksana <121404661+0xytoc1n@users.noreply.github.com>
2023-04-26 17:08:28 +0300

    ПР 7 первая итерация

---
+++ scripts/card.js
@@ -0,0 +1,45 @@
+export default class Card {
+    constructor(item, cardSelectorTemplate, openPicturePopup) {
+      this._item = item;
+      this._link = item.link;
+      this._name = item.name;
+      this._cardSelectorTemplate = cardSelectorTemplate;
+      this._openPicturePopup = openPicturePopup;
+    }
+  
+    _makeCards() {
+      return document.querySelector(this._cardSelectorTemplate).content.querySelector('.elements__list').cloneNode(true)
+    }
+  
+    _toggleLike = () => {
+      this._likeButton.classList.toggle('element__like_active');
+  }
+  
+    _deleteTrash = () => {
+      this._cloneElement.remove();
+      this._cloneElement = null;
+    }
+  
+    _openImagePopup = () => {
+      this._openPicturePopup(this._item)
+    }
+  
+    _addEventListener() {
+      this._likeButton.addEventListener('click', this._toggleLike);
+      this._trashButton.addEventListener('click', this._deleteTrash);
+      this._imageElement.addEventListener('click', this._openImagePopup);
+  }
+  
+    createCard() {
+      this._cloneElement = this._makeCards();
+      this._imageElement = this._cloneElement.querySelector('.element__image');
+      this._likeButton = this._cloneElement.querySelector('.element__like');
+      this._trashButton = this._cloneElement.querySelector('.element__trh-btn');
+      this._title = this._cloneElement.querySelector('.element__title');
+      this._imageElement.src = this._link;
+      this._imageElement.alt = this._name;
+      this._title.textContent = this._name;
+      this._addEventListener()
+      return this._cloneElement
+  }
+  };
---
+++ scripts/formValidator.js
@@ -0,0 +1,85 @@
+export default class FormValidator {
+  constructor(config, form) {
+    this._inputSelector = config.inputSelector;
+    this._submitButtonSelector = config.submitButtonSelector;
+    this._errorTemplateSelector = config.errorTemplateSelector;
+    this._disabledButtonClass = config.disabledButtonClass;
+    this._inputErrorClass = config.inputErrorClass;
+    this._errorClass = config.errorClass;
+    this._button = form.querySelector(this._submitButtonSelector);
+    this._inputLists = form.querySelectorAll(this._inputSelector);
+    this._form = form;
+  }
+
+  _hideInputError(errorElement, inputElement) {
+    inputElement.classList.remove(this._inputErrorClass);
+    errorElement.classList.remove(this._errorClass);
+    errorElement.textContent = "";
+  }
+
+  _showInputError(errorElement, inputElement) {
+    inputElement.classList.add(this._inputErrorClass);
+    errorElement.classList.add(this._errorClass);
+    errorElement.textContent = inputElement.validationMessage;
+  }
+
+  _hasInvalidInput() {
+    return Array.from(this._inputLists).some(
+      (inputElement) => !inputElement.validity.valid
+    );
+  }
+
+  _disabledButton() {
+    this._button.classList.add(this._disabledButtonClass);
+    this._button.disabled = true;
+  }
+
+  _enableButton() {
+    this._button.classList.remove(this._disabledButtonClass);
+    this._button.disabled = false;
+  }
+
+  _toggleButtonState() {
+    if (this._hasInvalidInput()) {
+      this._disabledButton(this._button);
+    } else {
+      this._enableButton();
+    }
+  }
+
+  _checkInputValidity(inputElement) {
+    const errorElement = this._form.querySelector(
+      `${this._errorTemplateSelector}${inputElement.id}`
+    );
+    if (inputElement.validity.valid) {
+      this._hideInputError(errorElement, inputElement);
+    } else {
+      this._showInputError(errorElement, inputElement);
+    }
+  }
+
+  _addEventListener() {
+    this._inputLists.forEach((inputElement) => {
+      inputElement.addEventListener("input", () => {
+        this._checkInputValidity(inputElement);
+        this._toggleButtonState();
+      });
+    });
+  }
+
+  enableValidation() {
+    this._addEventListener();
+  }
+
+  resetErrorForm() {
+    this._inputLists.forEach((inputElement) => {
+      const errorTemplate = this._form.querySelector(
+        `${this._errorTemplateSelector}${inputElement.id}`
+      );
+      if (!inputElement.validity.valid) {
+        this._hideInputError(errorTemplate, inputElement);
+      }
+    });
+    this._disabledButton();
+  }
+}
